f2632ddaf3daac7be4b88f9e16f151fca8282fa0
The Z Man <zachgabernathy@gmail.com>
2013-01-24 22:37:15 -0500

    Updating to Bootstrap 2.2.2

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -1,6 +1,3 @@
-// Avoid `console` errors in browsers that lack a console.
-(function(){var e;var t=function(){};var n=["assert","clear","count","debug","dir","dirxml","error","exception","group","groupCollapsed","groupEnd","info","log","markTimeline","profile","profileEnd","table","time","timeEnd","timeStamp","trace","warn"];var r=n.length;var i=window.console=window.console||{};while(r--){e=n[r];if(!i[e]){i[e]=t}}})()
-
 /*!
 * Bootstrap.js by @fat & @mdo
 * Copyright 2012 Twitter, Inc.
