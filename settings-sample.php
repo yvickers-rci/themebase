579a17055f3b62fbd69ad357aa2ee7f9058d5688
Yancey Vickers <yvickers@redclayinteractive.com>
2013-05-10 16:06:57 -0400

    Added server switch statement to have different settings per environment

--- settings-sample.php
+++ settings-sample.php
@@ -1,13 +1,20 @@
 <?php
-$themebase_settings = array(
-	'client_directory'=>'sample-client',
+switch($_SERVER['HTTP_HOST']){
+	/* live settings should be the default */
+	default:
+		$themebase_settings = array(
+			'client_directory'=>'sample-client',
 
-    'typekit_id'=>'',
-	'addthis_id'=>'',
-	'ga_id'=>'',
+		    'typekit_id'=>'',
+			'addthis_id'=>'',
+			'ga_id'=>'',
 
-    'image_sizes'=>array(
-		array('thumb-800x600', 800, 600, true),
-		array('thumb-480x320', 480, 320, true),
-	),
-);
+		    'image_sizes'=>array(
+				array('thumb-800x600', 800, 600, true),
+				array('thumb-480x320', 480, 320, true),
+			),
+		);
+	break;
+
+	/* dev environments */
+}
